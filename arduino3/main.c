e75c81428cb8f993bb68fe2b18f3e99fd0b3631a
20181106279 <1771478860@qq.com>
2019-12-10 15:55:08 +0800

    1
    1

--- arduino3/main.c
+++ arduino3/main.c
@@ -1,15 +1,21 @@
-//
-//  main.c
-//  arduino3
-//
-//  Created by 20181106279 on 2019/12/10.
-//  Copyright © 2019 20181106279. All rights reserved.
-//
+int e = 13;
+void setup() {
+    // put your setup code here, to run once:
+    u(e,OUTPUT);
+}
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+void loop() {
+    // put your main code here, to run repeatedly:
+    for(int i=0;i<3;i++){
+        digitalWrite(e,HIGH);
+        delay(200);
+        digitalWrite(e,LOW);
+        delay(200);
+    }
+    for(int i=0;i<3;i++){
+        digitalWrite(e,HIGH);
+        delay(2000);
+        digitalWrite(e,LOW);
+        delay(2000);
+    }
 }
